c01f051a0cc236d0290ddaeb035559d25152d151
Jose Luis Orozco <handark@gmail.com>
2022-09-02 22:00:11 -0500

    buscar pokemones

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,54 @@
 import { NextPage, GetStaticProps } from "next";
-import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { Grid, Input } from "@nextui-org/react";
 
 import { pokeApi } from "../api";
 import { Layout } from "../components/layouts";
 import { PokemonListResponse, SmallPokemon } from "../interfaces";
 import { PokemonCard } from "../components/pokemon";
+import { useEffect, useState } from "react";
 
 interface Props {
   pokemons: SmallPokemon[];
 }
 
 const HomePage: NextPage<Props> = ({ pokemons }) => {
+  const [search, setSearch] = useState("");
+
+  const [filteredPokemons, setFilteredPokemons] = useState(pokemons);
+
+  /**
+   * Filtra los pokemons por el nombre
+   */
+  useEffect(() => {
+    if (search === "") {
+      setFilteredPokemons(pokemons);
+      return;
+    }
+
+    const filteredPokemons = pokemons.filter((pokemon) =>
+      pokemon.name.includes(search)
+    );
+    setFilteredPokemons(filteredPokemons);
+  }, [search]);
+
   return (
     <Layout title="Listado de Pokémons">
+      <Grid.Container gap={2}>
+        <Grid xs={12}>
+          <Input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            fullWidth
+            placeholder="Buscar Pokémon"
+            aria-label="Buscar Pokémon"
+            onClearClick={() => setSearch("")}
+            clearable
+          />
+        </Grid>
+      </Grid.Container>
+
       <Grid.Container gap={2} justify="flex-start">
-        {pokemons.map((pokemon) => (
+        {filteredPokemons.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
         ))}
       </Grid.Container>
